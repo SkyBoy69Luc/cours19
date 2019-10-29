45b74055f95a69c010a57e7e2d47b2409189e14e
Skyboy69 <luc.plante.guitare.skyboy69@gmail.com>
2019-10-29 10:37:51 -0400

    Final Version

---
+++ ShaderNeige/Assets/Terrain/SnowTracks/WheelTrack.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelTrack : MonoBehaviour {
+
+    public Shader _drawShader;
+    public GameObject _terrain;
+    public Transform[] _whells;
+    RaycastHit _groundHit;
+    int _layerMask;
+    [Range(0, 4)]
+    public float _brushSize;
+    [Range(0, 1)]
+    public float _brushStrength;
+    public float distance = 3;
+
+    private RenderTexture splatMap;
+    private Material snowMaterial, drawMaterial;
+
+
+
+    // Use this for initialization
+    void Start () {
+        _layerMask = LayerMask.GetMask("Ground");
+        drawMaterial = new Material(_drawShader);
+        snowMaterial = _terrain.GetComponent<MeshRenderer>().material;
+        splatMap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat);
+        snowMaterial.SetTexture("_Splat", splatMap);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (Transform wheel in _whells)
+        {
+            if (Physics.Raycast(wheel.position, Vector3.down, out _groundHit, distance, _layerMask))
+            {
+                drawMaterial.SetVector("_Coordinate", new Vector4(_groundHit.textureCoord.x, _groundHit.textureCoord.y, 0, 0));
+                drawMaterial.SetFloat("_Strength", _brushStrength);
+                drawMaterial.SetFloat("_Size", _brushSize);
+                RenderTexture temp = RenderTexture.GetTemporary(splatMap.width, splatMap.height, 0, RenderTextureFormat.ARGBFloat);
+                Graphics.Blit(splatMap, temp);
+                Graphics.Blit(temp, splatMap, drawMaterial);
+                RenderTexture.ReleaseTemporary(temp);
+
+            }
+        }
+	}
+}
